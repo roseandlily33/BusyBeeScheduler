ed756c28f72ba472c418cbe165fe2f27d367ee60
Victoria Benoit <vrose834@gmail.com>
2023-02-13 13:10:11 -0400

    push

--- newscript.js
+++ newscript.js
@@ -2,15 +2,15 @@
 $(document).ready(function(){
     let currentHour = dayjs().hour();
     console.log(currentHour);
-    let time = currentHour;
+    //var entry = $('textarea');
+    
   
   $(function () {
     //Time and Date Display with dayjs:
     var timeAndDate = dayjs().format('ddd MMM DD, YYYY  hh:mm:ss a');
     $('#currentDay').text(timeAndDate);
     
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. 
+    //Goes through each of the time blocks and checks it against the currentHour to decide what colour it should be:
     for (i = 0; i < 9; i++){
         $('.time-block').each(function(){
             var blockHour = parseInt($(this).attr('id').split('-')[1]);
@@ -28,13 +28,21 @@ $(document).ready(function(){
             }
         });
     };
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    $('.time-block').on('click', function(){
-        localStorage.setItem('')
+
+    //When the save button is clicked it stores contents of entry in localStorage with the hour:
+    $('.saveBtn').on('click', function(){
+        
+        //entry = e.target.value;
+        var entry = $(this).next().val();
+        //I've tried: .children() - displays nothing in console.log, .children().children() - gives undefined at least, .find() -undefined, .next()-undefined, .siblings() -this really doesnt work, .parent()- nothing
+        //I've tried e.target; entry = e.target.value;
+        console.log(entry);
+        var hour = $(this).val();
+        console.log(hour);
+        localStorage.setItem(entry, hour);
   
     })
+    //Returns the saved elements from localStorage:
     function getElements(){
       var savedEntries = JSON.parse(localStorage(plannerEntries));
       if(savedEntries !== null){
---
+++ readme2.md
@@ -0,0 +1,28 @@
+#  Work Day Scheduler
+Challenge #5 is to create a simple calender
+
+## What was used:
+I used bootstrap, and jQuery plug-ins, with HTML, CSS, and JavaScript. 
+I decided to add custom colouring to make it pretty.
+
+## Images:
+Coming soon
+
+## Screenshots:
+Coming soon
+
+## Difficulties:
+-I wasn't sure if the HTML portion itself was meant to be dyanmically created so I asked BCS, and they said that I could do it like this.
+I had some difficulties with this like changing the colour
+
+## What I learned:
+What I learned from this project is how to use plug-ins and frameworks like bootstrap and how useful that it can be when it comes to building a project and the time that it saves. I also learned that 
+
+## Motivation and Why:
+What this project solves is a 
+
+## Usage:
+Select the coloured block by the time that you would like to schedule something in for, once completed select the save icon on the right hand side in the dark green box. 
+
+## Why it stands out:
+My project stands out because
